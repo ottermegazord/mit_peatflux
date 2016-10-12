4f7bc4661bdfb747abd2a0644c2632bfeff9ad2a
idaly666 <idaly.ali@live.fr>
2016-10-12 14:46:59 +0800

    Adds updated class_li840.py: Repository to communicate with Li840. Also adds time

--- Valve_Control/li840.py
+++ Valve_Control/li840.py
@@ -1,10 +1,12 @@
 import serial
 import xmlformatter
 from lxml import etree
+import time
+import datetime
 
 class li840:
 
-    def __init__(self, file_name, port, baudrate, time, file):
+    def __init__(self, port, baudrate, time):
         self.ser = serial.Serial(port, baudrate, timeout = time)
 
     def li840_readline(self):
@@ -13,21 +15,26 @@ class li840:
         output = self.ser.readline()
         return output
 
-    def li840_adddate(li840_xml):
+    def li840_adddate(self, li840_xml):
         doc = etree.parse(li840_xml)
         data_element = doc.find('data')
-        datetime = etree.SubElement(data_element, 'datetime')
-        datetime.text = datetime.datetime.now().isoformat()
+        datetimer = etree.SubElement(data_element, 'datetime')
+        datetimer.text = datetime.datetime.now().isoformat()
+	#print(datetimer.text)
+	#print(doc)
         return doc
 
-    def li840_pullnow(li840_raw, li840_timed):
+    def li840_pullnow(self, li840_raw, li840_timed):
         f1 = open(li840_raw, 'w')
         serial_output = self.li840_readline()
-        f1.write(serial_output + '\n')
+	serial_output_eol = serial_output + '\n'
+        f1.write(serial_output_eol)
         f1.close()
+
         doc = self.li840_adddate(li840_raw)
-        doc.write(open('li840_timed', 'a'))
-        f2 = open('li840_timed', 'a')
+        doc.write(open(li840_timed, 'a'))
+        f2 = open(li840_timed, 'a')
         f2.write('\n')
         f2.close()
+	
 

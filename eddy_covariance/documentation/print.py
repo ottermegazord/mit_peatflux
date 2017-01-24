5637133f87ee34d7c2686b3cadd71d6ea293a240
Idaly Ali <idaly.ali@live.fr>
2017-01-24 12:22:37 +0800

    Adds xml to csv conversion to directory

---
+++ eddy_covariance/documentation/print.py
@@ -0,0 +1,21 @@
+import os
+import xmlformatter
+
+fo = open("li840_timed2.xml", "r")
+num_lines = sum(1 for line in open('li840_timed2.xml'))
+fw = open("cleaned.xml", "a")
+formatter = xmlformatter.Formatter(indent="1", indent_char="\t", encoding_output="ISO-8859-1", preserve=["literal"])
+
+fw.write("<peatflux>")
+for z in range (0, num_lines):
+	try:
+		stringy = fo.readline() 
+		x = formatter.format_string(stringy)
+		print(x)
+		fw.write(x + '\n')
+	except:
+		pass
+fw.write("</peatflux>")
+fo.close()
+fw.close()
+

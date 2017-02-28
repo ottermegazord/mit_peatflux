83f1c532dd1f00f9e483b514de1aa52eacc49a35
idaly666 <idaly.ali@live.fr>
2017-02-28 10:51:56 +0800

    adds logging routine for ge50a mass flow controller

--- eddy_covariance/class_ge50A.py
+++ eddy_covariance/class_ge50A.py
@@ -2,32 +2,24 @@ import serial
 
 
 class Ge50a:
-    def __init__(self, port, baudrate, time, log_txt, address="254"):
+    def __init__(self, port, baudrate, time, address="254"):
         self.ser = serial.Serial(port, baudrate, timeout=time)
-        self.log_txt = log_txt
         self.address = address
 
     def ge50a_concatenator(self, comm):
-        return "@@@"+str(self.address)+comm+";FF"
+        return "@@@" + str(self.address) + comm + ";FF"
 
     def ge50a_readline(self):
         raw_output = self.ser.readline()
         output = raw_output.replace("@@@000ACK", "").replace(";FF", "")
         return output
 
-    def ge50a_writelog(self, stringer):
-        log = open(self.log_txt, 'a')
-        log.write(stringer)
-        log.close()
-
     def ge50a_readaddress(self):
         self.ser.write(bytes("@@@254CA?;FF".encode()))
         output = self.ge50a_readline()
         return output
 
-
     def ge50a_writecomm(self, comm):
-	self.ser.write(bytes(self.ge50a_concatenator(comm)).encode())
-	output = self.ge50a_readline().strip()
-	return output
-
+        self.ser.write(bytes(self.ge50a_concatenator(comm)).encode())
+        output = self.ge50a_readline().strip()
+        return output

040111f7f3834f9fd753fa66117448bd3f43a10b
Idaly Ali <idaly.ali@live.fr>
2017-02-28 03:12:43 +0000

    adds ge50a routine to peatflux-code repo

--- eddy_covariance/ge50a.py
+++ eddy_covariance/ge50a.py
@@ -8,18 +8,20 @@ timeout = 1
 address = "254"
 
 """Log Files"""
-log_txt = '/home/pi/Desktop/peatflux-code/eddy_covariance/ge50a_log.txt'
+log_txt = 'ge50a.txt'
 
 """Initialization"""
 ge50a = Ge50a('/dev/ttyGE50A', baudrate, timeout, address)
-f = open('log_txt', 'a')
 
 """Routine"""
 
 while 1:
     try:
+	f = open(log_txt, 'a')
         datetimer = datetime.datetime.now().isoformat()
-        f.write(datetimer+" "+ge50a.ge50a_writecomm("F?"))
+        output = datetimer+" "+ge50a.ge50a_writecomm("F?")+"\n"
+	f.write(output)
+	f.close()
 
     except:
         continue

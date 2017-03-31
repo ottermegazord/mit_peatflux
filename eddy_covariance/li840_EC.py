8bb113c479842381053f6235907d6ce0088eb71b
idaly666 <idaly.ali@live.fr>
2017-03-31 11:58:04 +0800

    reformat indentation

--- eddy_covariance/li840_EC.py
+++ eddy_covariance/li840_EC.py
@@ -31,7 +31,7 @@ for i in range(1, nodes + 1):
     files_timed.append("/home/pi/Desktop/peatflux-code/eddy_covariance/profile_nodes/li840_timed_%i.xml" % i)
     f.close()
 
-#  Calibration Log
+# Calibration Log
 log_txt = '/home/pi/Desktop/peatflux-code/eddy_covariance/profile_nodes/li840_log.xml'
 cal_txt = '/home/pi/Desktop/peatflux-code/eddy_covariance/profile_nodes/li840_cal.txt'
 
@@ -50,30 +50,31 @@ EDDY_CAL_CHAN = 16
 
 """Initialization"""
 test = li840(port, baudrate, timeout, SWITCH_OPEN, SWITCH_CLOSE, open_chan_list, close_chan_list, log_txt,
-                 cal_txt)
+             cal_txt)
 threeway = Threeway(THREE_WAY)
 valve = Valve(SWITCH_OPEN, SWITCH_CLOSE, open_chan_list, close_chan_list)
-
 
 """Routine"""
 while 1:
     dt = datetime.datetime.now()
     try:
-	
-	if (dt.hour == 11 and dt.minute == 00):
-	   threeway.open(2)
-	   valve.open_valve_channel(EDDY_CAL_CHAN, 0.25)
-	   test.li840_calibration(EC_channels, h2o_zero_interval, h2o_span_interval, co2_zero_interval, co2_span_interval, h2o_span, co2_span)
-           valve.close_valve_channel(EDDY_CAL_CHAN, 0.25)
-	   threeway.close(2)
-        elif dt.minute == 0 or dt.minute == 10 or dt.minute == 20 or dt.minute == 30 or dt.minute == 40 or dt.minute == 48 :
+
+        if (dt.hour == 11 and dt.minute == 00):
+            threeway.open(2)
+            valve.open_valve_channel(EDDY_CAL_CHAN, 0.25)
+            test.li840_calibration(EC_channels, h2o_zero_interval, h2o_span_interval, co2_zero_interval,
+                                   co2_span_interval, h2o_span, co2_span)
+            valve.close_valve_channel(EDDY_CAL_CHAN, 0.25)
+            threeway.close(2)
+
+        elif dt.minute == 0 or dt.minute == 10 or dt.minute == 20 or dt.minute == 30 or dt.minute == 40 or dt.minute == 48:
             print(dt.minute)
             for i in range(1, 9):
                 valve.open_valve_channel(i, 0.15)
-		print("channel %d\n" %i)
-                print(files_timed[i-1])
+                print("channel %d\n" % i)
+                print(files_timed[i - 1])
                 time.sleep(li840_read_period)
-                test.li840_pullnow(files_raw, files_timed[i-1])
+                test.li840_pullnow(files_raw, files_timed[i - 1])
                 valve.close_valve_channel(i, 0.15)
 
     except:

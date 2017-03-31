f2f0180b266bf04d6e2eb83aefba3b59eb200429
Idaly Ali <idaly.ali@live.fr>
2017-03-31 11:45:50 +0800

    adds V6 and V5 valves to eddy covariance programs

--- eddy_covariance/eddy_covariance.py
+++ eddy_covariance/eddy_covariance.py
@@ -14,13 +14,13 @@ baudrate = 115200
 timeout = 1
 
 """Calibration Constants"""
-h2o_zero_interval = 1
-h2o_span_interval = 1
-co2_zero_interval = 1
-co2_span_interval = 1
+h2o_zero_interval = 0.2
+h2o_span_interval = 0.2
+co2_zero_interval = 0.2
+co2_span_interval = 0.2
 co2_ref = 0
-co2_span = [350, 390, 430]
-h2o_span = [0, 0, 0]
+co2_span = [350, 0, 430]
+h2o_span = [0 ,0, 0]
 
 """Log Files"""
 log_txt = '/home/pi/Desktop/peatflux-code/eddy_covariance/li7000_log.txt'
@@ -37,12 +37,14 @@ SWITCH_CLOSE = 22
 SWITCH_INTERVAL = 0.5
 EC_channels = [9, 10, 11, 12]  # First element is zeroing Channel
 THREE_WAY = [13, 15]
+EDDY_CAL_CHAN = 15
 
 """Initialization"""
 test = li7000(port, baudrate, timeout, SWITCH_OPEN, SWITCH_CLOSE, open_chan_list, close_chan_list, log_txt,
               cal_txt)
 
 threeway = Threeway(THREE_WAY)
+valve = Valve(SWITCH_OPEN, SWITCH_CLOSE, open_chan_list, close_chan_list)
 
 """printing header file if log doesn not exist"""
 if (not os.path.isfile(log_txt)):
@@ -56,14 +58,16 @@ while 1:
 
     dt = datetime.datetime.now()
     try:
-        if (dt.hour == 8 and dt.minute == 17):
-	    threeway.open(2)	
+        if (dt.hour == 10 and dt.minute == 9):
+	    threeway.open(1)
+	    valve.open_valve_channel(EDDY_CAL_CHAN, 0.25)	
             test.li7000_calibration(EC_channels, h2o_zero_interval, h2o_span_interval, co2_zero_interval, co2_span_interval,
                                     h2o_span, co2_ref, co2_span)
-	    
-        else:
-	    threeway.close(2)
+	    valve.close_valve_channel(EDDY_CAL_CHAN, 0.25)
 	    threeway.close(1)
+        else:
+	    threeway.close(1)
+	    threeway.close(2)
             poll = test.li7000_pollnow()
 	    print(poll)
             test.li7000_writelog(poll)

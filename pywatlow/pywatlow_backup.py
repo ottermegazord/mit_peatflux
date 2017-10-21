482707c82aa813c6e6f951d28e836d70df6cf56c
ottermegazord <idaly.ali@live.fr>
2017-10-21 18:11:10 -0400

    Adds sample CSV

--- pywatlow/pywatlow_backup.py
+++ pywatlow/pywatlow_backup.py
@@ -27,10 +27,8 @@ class Watlow(minimalmodbus.Instrument):
         raw_temp1 = self.get_current_temperatureA()
         raw_temp2 = self.get_current_temperatureB()
         temp1 = hex(raw_temp1).zfill(4) + hex(raw_temp2)[2:].zfill(4)
-	if temp1 == '00x00000':
-                return 0
-        else:
-		return struct.unpack('>f', binascii.unhexlify(temp1[2:]))[0]
+	return temp1        
+	#return struct.unpack('>f', binascii.unhexlify(temp1[2:]))[0]
 
     def get_closed_loop_setpoint_A(self):
         return self.read_register(2161, functioncode=3)
@@ -42,10 +40,8 @@ class Watlow(minimalmodbus.Instrument):
         raw_sp1 = self.get_closed_loop_setpoint_A()
         raw_sp2 = self.get_closed_loop_setpoint_B()
         sp = hex(raw_sp1).zfill(4) + hex(raw_sp2)[2:].zfill(4)
-        if sp == '00x00000':
-                return 0
-        else:
-		return struct.unpack('>f', binascii.unhexlify(sp[2:]))[0]
+        return sp
+	#return struct.unpack('>f', binascii.unhexlify(sp[2:]))[0]
 
     def get_heat_power_A(self):
         return self.read_register(1905, functioncode=3)
@@ -56,20 +52,20 @@ class Watlow(minimalmodbus.Instrument):
     def get_heat_power(self):
         raw_heat1 = self.get_heat_power_A()
         raw_heat2 = self.get_heat_power_B()
-        heat = hex(raw_heat1).zfill(4) + hex(raw_heat2)[2:].zfill(4)
+	heat = hex(raw_heat1).zfill(4) + hex(raw_heat2)[2:].zfill(4)
 	if heat == '00x00000':
-                return 0
-        else:
+		return 0
+	else:
 		return struct.unpack('>f', binascii.unhexlify(heat[2:]))[0]
 
 if __name__ == '__main__':
     instrument = Watlow('/dev/ttyUSB0', 1, 9600)
-    file = open('pywatlow.csv', 'a')
+    file = open('pywatlow_test.csv', 'a')
     while 1:
-	try:
-		time.sleep(5)
+	#try:
+		#time.sleep(5)
         	dt = datetime.datetime.now()
         	file.write("%s,%s,%s,%s\n" % (dt,instrument.get_current_temperature(), instrument.get_closed_loop_setpoint(), instrument.get_heat_power()))
         	print ("%s,%s,%s,%s\n" % (dt, instrument.get_current_temperature(), instrument.get_closed_loop_setpoint(), instrument.get_heat_power()))
-	except:
-		None
+	#except:
+	#	None
